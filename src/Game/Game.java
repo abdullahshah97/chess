6082f722e19a306590438aacf4a35401ad7b9df3
Abdullah Shah <abdullah-shah97@hotmail.com>
2018-12-13 19:22:04 +0000

    Game class holds main method, initialising objects

---
+++ src/Game/Game.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+/**
+ *
+ * @author abdullah
+ */
+public class Game {
+    
+    public static void main(String [] args){
+        Board board = new Board();
+    }
+}
